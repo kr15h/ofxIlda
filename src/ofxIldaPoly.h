2f161ef16f00379b0924728f20b6db7fb1e93052
Krisjanis Rijnieks <krisjanis.rijnieks@gmail.com>
2020-12-31 18:58:50 +0200

    Up ofPoint to glm::vec2

--- src/ofxIldaPoly.h
+++ src/ofxIldaPoly.h
@@ -9,23 +9,23 @@
 #pragma once
 
 namespace ofxIlda {
-    
+
 	class Poly: public ofPolyline{
 	public:
 		ofFloatColor color;
 		Poly() : color(ofFloatColor(1, 1, 1, 1)) {}
 
         Poly(ofFloatColor color) : color(color) {}
-        
+
         Poly(const Poly& poly) : ofPolyline(poly), color(poly.color) {}
-        
+
         Poly(const ofPolyline& polyline) : ofPolyline(polyline), color(ofFloatColor(1, 1, 1, 1)) {}
-        
+
         Poly(const ofPolyline& polyline, ofFloatColor color) : ofPolyline(polyline), color(color) {}
 
-		Poly(const vector<ofPoint>& verts) : ofPolyline(verts), color(ofFloatColor(1, 1, 1, 1)) {}
+		    Poly(const vector<glm::vec2>& verts) : ofPolyline(verts), color(ofFloatColor(1, 1, 1, 1)) {}
 
-		Poly(const vector<ofPoint>& verts, ofFloatColor color) : ofPolyline(verts), color(color) {}
+		    Poly(const vector<glm::vec2>& verts, ofFloatColor color) : ofPolyline(verts), color(color) {}
         
         void setFromPolyline(const ofPolyline& polyline) { ofFloatColor tmpColor = color; *this = polyline; color = tmpColor; }
 	};
